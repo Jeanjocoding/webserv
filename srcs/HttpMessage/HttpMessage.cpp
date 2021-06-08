b281f4f02dc24c224cd2f7948f778ac155b4a98f
jeanjocoding <theophile.lucille@gmail.com>
2021-06-08 19:59:47 +0200

    Http in progress

--- srcs/HttpMessage/HttpMessage.cpp
+++ srcs/HttpMessage/HttpMessage.cpp
@@ -10,6 +10,11 @@ HttpMessage::HttpMessage(HttpMessage const& to_copy)
 	(*this) = to_copy;
 }
 
+HttpMessage::HttpMessage(std::string str_message): _stringMessage(str_message)
+{
+	return;
+}
+
 HttpMessage::~HttpMessage(void)
 {
 	
@@ -19,7 +24,6 @@ HttpMessage&	HttpMessage::operator=(HttpMessage const& to_copy)
 {
 	_headers = to_copy._headers;
 	_body = to_copy._body;
-	_method_status = to_copy._method_status;
 	return (*this);
 
 }
--- srcs/HttpMessage/HttpRequest.cpp
+++ srcs/HttpMessage/HttpRequest.cpp
@@ -13,6 +13,11 @@ HttpRequest::HttpRequest(HttpRequest const& to_copy) : HttpMessage(to_copy)
 	_methodArgument = to_copy._methodArgument;
 }
 
+HttpRequest::HttpRequest(std::string str_message) : HttpMessage(str_message)
+{
+	parseFromString(_stringMessage);
+}
+
 HttpRequest::~HttpRequest(void)
 {
 	
@@ -28,15 +33,31 @@ HttpRequest&	HttpRequest::operator=(HttpRequest const& to_copy)
 	return (*this);
 }
 
+/* probablement optimisable sans trop de difficulté en cas de besoin */
 int				HttpRequest::parseFromString(std::string str_request)
 {
-	int pos_body = str_request.find("\r\n\r\n");
+	size_t pos_body = str_request.find("\r\n\r\n");
 
 	std::string head_block = str_request.substr(0, pos_body);
+	std::cout << "head_block: " << head_block << std::endl;
 
 	if (pos_body + 4 < str_request.length())
-	_body = str_request.substr(pos_body + 3);
+	{
+		_body = str_request.substr(pos_body + 4);
+		std::cout << "body: " << _body << std::endl;
+	}
+	size_t		pos_runner = head_block.find("\r\n");
+	_methodLine = head_block.substr(0, pos_runner);
+	std::cout << "method line: " << _methodLine << std::endl;
 
-
-
+	std::string	header_string = head_block.substr(pos_runner);
+	std::cout << "header string: " << header_string << std::endl;
+	return (0);	
+//	_parseHeaderBlock(header_string);
 }
+
+std::string const&		HttpRequest::toString() const
+{
+	return (_stringMessage);
+}
+
