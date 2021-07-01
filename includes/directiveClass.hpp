4b44389f1ec1cf6a47f36840908d940a917b845e
user42 <sablayrolles.antoine@gmail.com>
2021-07-01 15:49:48 +0200

    proper management of location

--- includes/directiveClass.hpp
+++ includes/directiveClass.hpp
@@ -6,7 +6,7 @@
 /*   By: asablayr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 14:11:07 by asablayr          #+#    #+#             */
-/*   Updated: 2021/06/21 18:25:41 by asablayr         ###   ########.fr       */
+/*   Updated: 2021/06/28 13:08:26 by asablayr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,20 @@
 #include <map>
 #include <string>
 
-#define ON_OFF	1
-#define	STRING	2
-#define	INT		3
-#define	BLOCK	4
+#define	SYNTAX_ON_OFF		1
+#define	SYNTAX_STRING		2
+#define	SYNTAX_FILE			3
+#define	SYNTAX_BLOCK		4
+#define	SYNTAX_SIZE			5
+#define	SYNTAX_NUMBER		6
+#define	SYNTAX_NUMBER_SIZE	7
+#define	SYNTAX_TIME			8
+#define	SYNTAX_PATH			9
+#define	SYNTAX_ALL_ANY		10
+#define	SYNTAX_NAME			11
+#define	SYNTAX_ADDRESS		12
+#define	SYNTAX_RATE			13
+#define	SYNTAX_CODE_URI		14
 
 class directiveClass
 {
@@ -33,7 +43,8 @@ class directiveClass
 
 			~directiveClass();
 
-			bool	isInContext(std::string);
+			bool	isInContext(std::string const& context_name) const;
+			bool	parse(std::string);
 
 			std::string								_name;
 			std::vector<std::string>				_contexts;
