2c974ec8da921b249deaf6d2d93511f31b14e238
jeanjocoding <theophile.lucille@gmail.com>
2021-09-04 17:02:36 +0200

    fix second mismatch bug

--- srcs/CgiLauncher/cgiLauncher.cpp
+++ srcs/CgiLauncher/cgiLauncher.cpp
@@ -129,7 +129,7 @@ int		launchCgiScript(t_CgiParams& params, HttpRequest const& request, LocationCl
 			append_to_buffer(output, buffer_size, read_buffer, read_ret);
 		}
 		close (script_output_pipe[0]);
-		delete args[0];
+		delete [] args[0];
 		delete [] args;
 		if (read_ret == -1)
 		{
