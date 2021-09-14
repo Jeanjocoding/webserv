6c0e990640e494729b7e418b18928f730e5b471a
user42 <sablayrolles.antoine@gmail.com>
2021-09-14 14:13:31 +0200

    add duplicate location check

--- srcs/setup/setup_server.cpp
+++ srcs/setup/setup_server.cpp
@@ -6,7 +6,7 @@
 /*   By: asablayr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 19:42:54 by asablayr          #+#    #+#             */
-/*   Updated: 2021/08/17 22:18:54 by asablayr         ###   ########.fr       */
+/*   Updated: 2021/09/14 14:09:01 by asablayr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,19 @@
 #include <cstdlib>
 #include "serverClass.hpp"
 #include "webserv.hpp"
+
+static std::string	check_double_location(serverClass const& server)
+{
+	for (std::vector<LocationClass*>::const_iterator it = server._location.begin(); it != --(server._location.end()); it++)
+	{
+		for (std::vector<LocationClass*>::const_iterator i = it + 1; i != server._location.end(); i++)
+		{
+			if ((*it)->getUri() == (*i)->getUri() && (*it)->getParam() == (*i)->getParam())
+				return (*it)->getUri();
+		}
+	}
+	return "";
+}
 
 static void	input_context(contextClass const& context, serverClass& base_server, std::vector<serverClass*>& vector_server)
 {
@@ -65,6 +78,16 @@ std::vector<serverClass*>	setup_server(std::string conf_file)
 		if ((*it)->_location.empty())
 			(*it)->_location.push_back(new LocationClass());
 	for (std::vector<serverClass*>::iterator it = server_map.begin(); it != server_map.end(); it++)
+	{
 		(*it)->setLocation();
+		std::string tmp = check_double_location(**it);
+		if (!tmp.empty())
+		{
+			std::cerr << "duplicate location " << tmp << " in server " << (*it)->_server_name << std::endl;
+			for (std::vector<serverClass*>::iterator it = server_map.begin(); it != server_map.end(); it++)
+				delete *it;
+			exit(EXIT_FAILURE);
+		}
+	}	
 	return (server_map);
 }
